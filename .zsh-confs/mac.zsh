bd5356ad78b5064da2e25655673169a3c5b0a903
Daniel Luna <dancluna@gmail.com>
2020-01-10 16:09:19 -0300

    nvm homebrew setup

--- .zsh-confs/mac.zsh
+++ .zsh-confs/mac.zsh
@@ -8,3 +8,6 @@ export CRONITOR_CONFIG="/Users/dluna/.cronitor.json"
 alias eman='PAGER=less man -P eless'
 
 eval "$(fasd --init auto)"
+
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
