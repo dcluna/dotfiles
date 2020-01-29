e3b7fe8a90b1bb0ad62baadbb36563a795e29d15
Daniel Luna <dancluna@gmail.com>
2020-01-29 15:53:14 -0300

    Adding asdf and recordit

--- .zsh-confs/mac.zsh
+++ .zsh-confs/mac.zsh
@@ -11,3 +11,6 @@ eval "$(fasd --init auto)"
 
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+. $(brew --prefix asdf)/asdf.sh
+. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
