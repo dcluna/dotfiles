170d6f46682ef01767a759ac321f08828c6f7964
Daniel Luna <daniel@adquick.com>
2020-08-24 10:06:15 -0300

    bq_load_parquet

--- .zsh-confs/functions.zsh
+++ .zsh-confs/functions.zsh
@@ -5,6 +5,10 @@ function bq_load_csv_autodetect() {
             $1 $2
 }
 
+function bq_load_parquet() {
+    bq load --source_format=PARQUET $1 $2
+}
+
 function visidata_csv_to_trend() {
     cat $1 | jq "reduce .[] as \$item ({}; . + {(\$item.${2:-day}): \$item.${3:-count}})"
 }
