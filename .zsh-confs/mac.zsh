9d1cef0b2793e7ef4716bf3e917522b32913d414
Daniel Luna <dancluna@gmail.com>
2020-10-01 12:03:59 -0300

    Remove unnecessary asdf loading, remove fasd

--- .zsh-confs/mac.zsh
+++ .zsh-confs/mac.zsh
@@ -7,12 +7,9 @@ export PATH="$PATH:$HOME/.bin"
 export CRONITOR_CONFIG="/Users/dluna/.cronitor.json"
 alias eman='PAGER=less man -P eless'
 
-eval "$(fasd --init auto)"
-
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-. $(brew --prefix asdf)/asdf.sh
-. $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
+# already loaded in https://github.com/dcluna/dotfiles/blob/022618eee2cbe1faace2486cd374e1837ac81f2e/.zshrc#L153
 
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
