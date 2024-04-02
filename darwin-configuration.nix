bcbaca20818d363526ae17e41ba09e8ae5d3f362
Daniel Luna <dancluna@gmail.com>
2024-04-02 15:22:52 -0300

    Installing the postgres service with mostly everything I need for local dev

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -7,6 +7,52 @@
     [ pkgs.vim
     ];
 
+  nix.settings.experimental-features = [ "nix-command" ];
+
+  # taken from https://github.com/LnL7/nix-darwin/issues/339
+  system.activationScripts.preActivation = {
+    enable = true;
+    text = ''
+      if [ ! -d "/var/lib/postgresql/" ]; then
+        echo "creating PostgreSQL data directory..."
+        sudo mkdir -m 750 -p /var/lib/postgresql/
+        chown -R danielluna:staff /var/lib/postgresql/
+      fi
+    '';
+  };
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_15;
+    extraPlugins = with pkgs.postgresql_15.pkgs; [ postgis h3-pg pgvector ];
+    initdbArgs = ["-U danielluna" "--pgdata=/var/lib/postgresql/15" "--no-locale" "--encoding=UTF8"];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database  DBuser  auth-method optional_ident_map
+      local all       all     peer        map=superuser_map
+      #type database  user    address   method
+      host  all       all     localhost trust
+    '';
+    identMap = ''
+       # ArbitraryMapName systemUser  DBUser
+       superuser_map      root        postgres
+       superuser_map      postgres    postgres
+       superuser_map      danielluna  postgres
+       # Let other names login as themselves
+       superuser_map      /^(.*)$     \1
+    '';
+    # ensureDatabases = [ "postgres" ]; # doesnt work on nix-darwin
+    # initialScript = pkgs.writeText "backend-initScript" '' # doesnt work on nix-darwin
+    #   CREATE ROLE postgres CREATEDB;
+    #   CREATE DATABASE postgres;
+    #   GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
+    # '';
+  };
+
+  launchd.user.agents.postgresql.serviceConfig = {
+    StandardErrorPath = "/Users/danielluna/nix-postgres/postgres.error.log";
+    StandardOutPath = "/Users/danielluna/nix-postgres/postgres.log";
+  };
+
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
