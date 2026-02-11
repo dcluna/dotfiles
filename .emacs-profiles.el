cf7a017302ffb87e8cf3005d4298495d57468a1e
Daniel Luna <dancluna@gmail.com>
2026-02-11 15:32:35 -0300

    Chemacs2: Add config for spacemacs and emacs-kick

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,6 +1,2 @@
-(
- ("default" . ((user-emacs-directory . "~/.emacs.default")))
- ("doom" . ((user-emacs-directory . "~/.doom-emacs")
-            (env . (("DOOMDIR" . "~/.doom.d"))))))
-;;; git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.doom-emacs
-;;; ln -s "/Users/danielluna/.ghq/github.com/dcluna/dotfiles/.emacs-profiles.el" .emacs-profiles.el
+(("spacemacs" . ((user-emacs-directory . "~/.emacs.spacemacs")))
+ ("kick" . ((user-emacs-directory . "~/.emacs-kick"))))
