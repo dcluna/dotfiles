56d539e5bbe7606adfd3473bd476c3fab79accbe
Daniel Luna <dluna@homeroom.com>
2026-03-05 14:17:17 -0300

    vagrant-dir-locals.el: use direnv in rails commands within the VM

--- homeroom/vagrant-dir-locals.el
+++ homeroom/vagrant-dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables         -*- no-byte-compile: t; -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((ruby-mode . ((projectile-rails-custom-console-command . "vagrant ssh -c \"cd /vagrant/api && bundle exec rails c\"")
+((ruby-mode . ((projectile-rails-custom-console-command . "vagrant ssh -c \"cd /vagrant/api && direnv exec . bundle exec rails c\"")
                (robe-ruby-path . "/vagrant/.robe-lib")
                (robe-port . "33315")
                (eval . (setq inf-ruby-first-prompt-pattern
@@ -39,6 +39,45 @@
                                 (locate-dominating-file default-directory "Vagrantfile")))
                          (advice-add 'rspec-vagrant-p :around
                                      #'vagrant-devenv-api--rspec-vagrant-p-advice)))
+               (eval . (progn
+                         (defun vagrant-devenv-api--rspec-vagrant-wrapper-advice (orig-fn command)
+                           "Inject direnv exec . into rspec command inside Vagrant."
+                           (if (rspec-vagrant-p)
+                               (format "vagrant ssh -c 'cd %s; direnv exec . %s'"
+                                       (shell-quote-argument rspec-vagrant-cwd)
+                                       command)
+                             (funcall orig-fn command)))
+                         (advice-add 'rspec--vagrant-wrapper :around
+                                     #'vagrant-devenv-api--rspec-vagrant-wrapper-advice)))
+               (robe-vagrant-path-mappings . (("/vagrant/api/" . "/Users/danielluna/Projects/vagrant-devenv/api/")
+                                              ("/vagrant/frontend/" . "/Users/danielluna/Projects/vagrant-devenv/frontend/")))
+               (eval . (progn
+                         (defun vagrant-devenv--translate-robe-path (file)
+                           "Translate a Vagrant VM file path to the corresponding host path.
+Uses `robe-vagrant-path-mappings' alist to find a matching prefix."
+                           (if (and (boundp 'robe-vagrant-path-mappings) robe-vagrant-path-mappings)
+                               (let ((mappings robe-vagrant-path-mappings)
+                                     (result file))
+                                 (while mappings
+                                   (let* ((mapping (car mappings))
+                                          (vm-prefix (car mapping))
+                                          (host-prefix (cdr mapping)))
+                                     (when (string-prefix-p vm-prefix file)
+                                       (setq result (concat host-prefix (substring file (length vm-prefix)))
+                                             mappings nil)))
+                                   (setq mappings (cdr mappings)))
+                                 result)
+                             file))
+                         (defun vagrant-devenv--robe-doc-format-file-name-advice (orig-fn file)
+                           "Translate VM path to host path before formatting for robe-doc display."
+                           (funcall orig-fn (vagrant-devenv--translate-robe-path file)))
+                         (advice-add 'robe-doc-format-file-name :around
+                                     #'vagrant-devenv--robe-doc-format-file-name-advice)
+                         (defun vagrant-devenv--robe-find-file-advice (orig-fn file &rest args)
+                           "Translate VM path to host path before opening the file."
+                           (apply orig-fn (vagrant-devenv--translate-robe-path file) args))
+                         (advice-add 'robe-find-file :around
+                                     #'vagrant-devenv--robe-find-file-advice)))
                (eval . (progn
                          (defun vagrant-devenv-api--bundle-command-advice (orig-fn cmd)
                            "Wrap bundle-command to run inside the Vagrant container."
