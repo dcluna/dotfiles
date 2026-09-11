9d595dcd18d294de939c038b790e44664b5bac99
Daniel Luna <dluna@homeroom.com>
2026-09-11 15:16:49 -0300

    reproduce_failure.rb: add '--focus' option

--- homeroom/scripts/reproduce_failure.rb
+++ homeroom/scripts/reproduce_failure.rb
@@ -8,14 +8,17 @@
 #
 # Usage:
 #   bin/reproduce_failure path/to/results.xml
-#   bin/reproduce_failure path/to/results.xml --full  # include all files, not just up to failure
+#   bin/reproduce_failure path/to/results.xml --full           # include all files, not just up to failure
+#   bin/reproduce_failure path/to/results.xml --focus spec/x.rb  # stop at this file's failure, not the first
 
 require "rexml/document"
 
 xml_path = ARGV.find { |a| !a.start_with?("--") }
 full_mode = ARGV.include?("--full")
+focus_idx = ARGV.index("--focus")
+focus_file = focus_idx && ARGV[focus_idx + 1]&.sub(%r{\A\./}, "")
 
-abort "Usage: #{$PROGRAM_NAME} <junit-xml-path> [--full]" unless xml_path
+abort "Usage: #{$PROGRAM_NAME} <junit-xml-path> [--full] [--focus spec/file.rb]" unless xml_path
 abort "File not found: #{xml_path}" unless File.exist?(xml_path)
 
 doc = REXML::Document.new(File.read(xml_path))
@@ -33,8 +36,11 @@ doc.each_element("testsuite/testcase") do |tc|
   files << file
 
   if !full_mode && tc.elements["failure"]
-    failure_file = file
-    break
+    # --focus: only stop at the focused file's failure
+    if focus_file.nil? || file == focus_file
+      failure_file = file
+      break
+    end
   end
 end
 
